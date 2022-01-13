2d562651881f3793e5ce3d0ee96926dc4051c539
Fabrice Hafashimana <feytoninc@hotmail.com>
2022-01-13 22:58:12 +0200

    Add testing modules to the application with Mocha
    With Mocha, I add the test for two modules
    And more tests will be added

--- src/test/blogTest.js
+++ src/test/blogTest.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 import * as blogModels from "../blogApp/models.js";
 
 // Testing modules
-import chai from "chai";
+import chai, { expect } from "chai";
 import chaiHttp from "chai-http";
 import * as base from "../index.js";
 
@@ -13,24 +13,49 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 
-describe("books", () => {
-  beforeEach((done) => {
-    blogModels.blogModel.remove({}),
-      (err) => {
-        done();
-      };
+describe("blog", function () {
+  beforeEach(async function () {
+    try {
+      await blogModels.blogModel.deleteMany({});
+    } catch (error) {
+      throw error;
+    }
   });
   //Testing get
 
-  describe("GET posts", () => {
-    it("it should GET all the posts", (done) => {
+  describe("GET Blog", () => {
+    it("it should return an object and No posts array", function (done) {
+      try {
+        chai
+          .request(base.app)
+          .get("/blog")
+          .end(function (err, res) {
+            if (err) {
+              throw err;
+            }
+
+            res.should.have.status(201);
+            res.body.should.be.a("object");
+            done();
+          });
+      } catch (error) {
+        throw error;
+      }
+    });
+  });
+  describe("POST Blog", () => {
+    it("It should return An forbidden request", function (done) {
+      let post = {
+        title: "The new macbook pro hello author 2",
+        content: "Here is content",
+        summary: "Here goes our summary",
+      };
       chai
         .request(base.app)
-        .get("/blog")
+        .post("/blog")
+        .send(post)
         .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("array");
-          res.body.length.should.be.eql(0);
+          res.should.have.status(401);
           done();
         });
     });
