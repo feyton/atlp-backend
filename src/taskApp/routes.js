5e0654086c69acafb5f880ff527a0df6a94fd8ea
Fabrice Hafashimana <feytoninc@hotmail.com>
2022-01-31 13:05:26 +0200

    Add validations to task routes

--- src/taskApp/routes.js
+++ src/taskApp/routes.js
@@ -2,11 +2,14 @@ import { Router } from "express";
 import { asyncHandler } from "../config/utils.js";
 import { checkObjectId } from "../userApp/middleware.js";
 import { verifyJWT } from "../userApp/utils.js";
+import { validate } from "../userApp/validator.js";
+import { taskCreateRules } from "./validator.js";
 import * as taskViews from "./views.js";
+
 const router = Router();
 
 router.get("/", verifyJWT, asyncHandler(taskViews.getTasks));
-router.post("/", verifyJWT, asyncHandler(taskViews.addTask));
+router.post("/", verifyJWT, taskCreateRules(), validate, asyncHandler(taskViews.addTask));
 router.put(
   "/:id",
   verifyJWT,
